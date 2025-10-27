13d12cb6980754c2e16ca47487f007e2b4bff9a3
Villanuev-a <jhvmalavazi@gmail.com>
2025-10-27 13:31:37 -0300

    resolvendo conflito

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+ const mongoSchema = mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+ });
+
+ mongoSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_PRIVATE_KEY);
+    return token;
+ }
+
+ const User = mongoose.model('User', mongoSchema);
+
+
+ function validateUser(user){
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+    });
+    return schema.validate(user);
+ }
+
+ module.exports ={
+    User,
+    validateUser,
+ }
