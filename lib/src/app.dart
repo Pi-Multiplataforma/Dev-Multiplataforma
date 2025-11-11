50e10fe4e00f34c0fc26c0d22711fac442e39cdb
Yuuki-ikuuY <131804011+Yuuki-ikuuY@users.noreply.github.com>
2025-11-11 15:00:53 -0300

    Dependencias adicionadas

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pi_dev_multiplataforma/mobile_homescreen.dart';
 import 'package:pi_dev_multiplataforma/src/telas/conversa_ia_mobile.dart';
 import '../src/telas/home_page.dart';
 import 'package:get/get.dart';
 import '../utilities/dependencies.dart' as dependencies;
 import '../src/telas/conversa_ia.dart';
+import 'package:flutter_device_type/flutter_device_type.dart';
+import 'dart:io';
 
 
 class App extends StatelessWidget{
@@ -17,8 +20,9 @@ class App extends StatelessWidget{
   initialBinding: dependencies.InitialBindings(),
   initialRoute: '/',
   getPages: [
-    GetPage(name: '/', page: () => ConversaIaMobile()),
+    GetPage(name: '/home_page', page: () => HomePage()),
     GetPage(name: '/conversa_ia', page: () => ConversaIa()), 
+    GetPage(name: '/mobile_home', page: () => MobileHome())
   ],
 );
 }
